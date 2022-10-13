e665ee81a178128bbbd749f6b14ef20973d1e13a
EdenMol <edenmolina@gmail.com>
2022-10-13 06:12:24 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,40 @@
+﻿using CalculatorApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Threading.Tasks;
+
+namespace CalculatorApi
+{
+    public class Calculator : IController
+    {
+        public Task<ApiResponse> SetCalculatedAsync(double valueA, double valueB, Op op)
+        {
+            return Task.Run(() =>
+            {
+                var temp = new ApiResponse();
+               
+                switch (op)
+                {
+                    case Op.Minus:
+                        temp.Result = valueA - valueB;
+                        break;
+                    case Op.Plus:
+                        temp.Result = valueA + valueB;
+                        break;
+                    case Op.Multiply:
+                        temp.Result = valueA * valueB;
+                        break;
+                    case Op.Divide:
+                        temp.Result = valueA / valueB;
+                        break;
+                        default: throw new ArgumentOutOfRangeException(nameof(op));
+                }
+                // TODO validate valueB not zero in Divide
+                return temp;
+            });
+        }
+
+       
+    }
+}
